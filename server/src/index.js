77a7cfe68e3a25bcafcc9afb477e3a79df753875
DimaTrue <montifick@rambler.ru>
2020-07-08 16:54:18 +0300

    eslint prettier setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 require('dotenv').config();
 
-app.get("/", function (req, res) {
-  res.send("Hello Word");
+app.get('/', function (req, res) {
+  res.send('Hello Word');
 });
 
 const port = process.env.PORT || 4001;
 app.listen(port, function () {
-  console.log("Server Running", port);
+  console.log('Server Running', port);
 });
